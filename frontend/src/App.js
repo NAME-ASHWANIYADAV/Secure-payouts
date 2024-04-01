45efa8b466735390e6857b42728198b5354f8d78
ashwani yadav <22ashwaniyadav@gmail.com>
2024-04-02 05:00:34 +0530

    updating the code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
-import StripePaymentForm from './StripePaymentForm'; // Update import for payment form
+import StripePaymentForm from './StripePaymentForm';
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
-
 import Header from './header.js';
+
+const stripePromise = loadStripe('pk_test_51OxwfiSHo9QORuto9btV2wKOgNIK65CLOuspbU62EMLhmPIXSHkWTU1vamFipbwQfUaLH2j2XlYct4oNLSmQprre00dNz6driG');
+
 const App = () => {
   return (
     <Router>
@@ -15,7 +17,11 @@ const App = () => {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/donate" element={<StripePaymentForm />} /> {/* Update route to use StripePaymentForm */}
+          <Route path="/donate" element={
+            <Elements stripe={stripePromise}>
+              <StripePaymentForm />
+            </Elements>
+          } />
         </Routes>
       </div>
     </Router>
