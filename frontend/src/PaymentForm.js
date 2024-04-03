c30a1264f1e885a7d80528ffd8f2231e2a6142ee
ashwani yadav <22ashwaniyadav@gmail.com>
2024-04-04 02:28:12 +0530

    adding so many things

--- frontend/src/PaymentForm.js
+++ frontend/src/PaymentForm.js
@@ -8,77 +8,7 @@ const PaymentForm = ({ history }) => {
   const [paymentStatus, setPaymentStatus] = useState('');
   const [message, setMessage] = useState('');
 
-  // const handleSubmit = async (e) => {
-  //   e.preventDefault();
-  //   try {
-  //     // Generate a random payment ID (you can use any method to generate a unique ID)
-  //     const paymentId = Math.random().toString(36).substr(2, 9);
-
-      
-
-  //     setMessage(response.data.message);
-  //     setPaymentStatus(response.data.payment.paymentStatus);
-
-  //     // Redirect to StripePaymentForm.js after processing the payment
-  //     history.push('/stripe-payment', { paymentId, paymentAmount });
-  //   } catch (error) {
-  //     setMessage('Payment failed. Please try again.');
-  //   }
-  // };
-
-  // payment integration
-  // const makePayment = async() =>{
-  //   const stripe = await loadStripe("pk_test_51OxwfiSHo9QORuto9btV2wKOgNIK65CLOuspbU62EMLhmPIXSHkWTU1vamFipbwQfUaLH2j2XlYct4oNLSmQprre00dNz6driG")
-  //   const body = {
-  //     amount: paymentAmount
-  //   }
-  //   const headers = {
-  //     "Content-Type":"application/json"
-  //   }
-  //   const response = await axios.post("http://localhost:7000/api/create-checkout-session",{
-  //     method: "POST",
-  //     headers:headers,
-  //     body:JSON.stringify(body)
-  //   });
-
-  //  const session = await response.json();
-
-  //  const result = stripe.redirectToCheckout({
-  //   sessionId:session.id
-  //  });
-
-  //  if(result.error){
-  //   console.log(result.error);
-  //  }
-  // }
-
-//   const makePayment = async () => {
-//     const body = {
-//         amount: parseFloat(paymentAmount), // Convert to float if needed
-//     };
-
-//     try {
-//         const response = await axios.post(
-//             "http://localhost:7000/api/create-checkout-session",
-//             body
-//         );
-
-//         const { id } = response.data;
-//         // Redirect to Stripe Checkout
-//         const stripe = await loadStripe(
-//             "pk_test_your_stripe_publishable_key"
-//         );
-//         const { error } = await stripe.redirectToCheckout({ sessionId: id });
-
-//         if (error) {
-//             console.error("Error redirecting to Checkout:", error);
-//             // Handle error
-//         }
-//     } catch (error) {
-//         console.error("Error making payment:", error);
-//         // Handle error
-//     }
-// };
+ 
 const makePayment = async () => {
   // Convert paymentAmount to an array with a single element
   const amountArray = [parseFloat(paymentAmount)];
--- frontend/src/cancel.js
+++ frontend/src/cancel.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import './cancel.css';
 
 function cancel() {
   return (
